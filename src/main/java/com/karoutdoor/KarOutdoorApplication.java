e9f526f675c5ee8ff94a674be785dae71fe2a72a
nihat <nihat.eymen.1907@gmail.com>
2024-10-23 10:42:56 +0300

    Initial commit

---
+++ src/main/java/com/karoutdoor/KarOutdoorApplication.java
@@ -0,0 +1,13 @@
+package com.karoutdoor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarOutdoorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KarOutdoorApplication.class, args);
+    }
+
+}
